3b095441415654e6de58bcf70c06693da17070fd
AadityaSharma1001 <b23me1001@iitj.ac.in>
2025-07-03 00:30:03 +0530

    minor fix

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+
 import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import { usePortfolioStore } from "@/store/portfolio";
